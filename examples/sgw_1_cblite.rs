42d1bdca2fcc2bd31c8a4b2394f2ce99c4754d0f
Antoine Mencière <95620255+Nao-ris@users.noreply.github.com>
2025-10-24 10:41:03 +0200

    Fix example compil

--- examples/sgw_1_cblite.rs
+++ examples/sgw_1_cblite.rs
@@ -43,6 +43,7 @@ fn main() {
         document_ids: MutableArray::default(),
         collections: None,
         accept_parent_domain_cookies: false,
+        #[cfg(feature = "enterprise")]
         accept_only_self_signed_server_certificate: false,
     };
     let repl_context = ReplicationConfigurationContext::default();
