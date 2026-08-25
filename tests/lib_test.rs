9b7c6b7b1f70a2d9409e577323410e535dce44fe
Damien Voreiter <damien.voreiter@doctolib.com>
2026-08-25 10:40:59 +0200

    build: Update cblite c to 3.2.6

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -6,5 +6,5 @@ use couchbase_lite::*;
 
 #[test]
 fn couchbase_lite_c_version_test() {
-    assert_eq!(couchbase_lite_c_version(), "3.2.4".to_string());
+    assert_eq!(couchbase_lite_c_version(), "3.2.6".to_string());
 }
